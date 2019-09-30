316cf50503d4a2d17c073a0bf1d5d240094cfc06
Dakota Gravitt <dakotagrvtt50@gmail.com>
2019-09-30 12:45:14 -0500

    Validation and proper output added and fixed

--- main.js
+++ main.js
@@ -11,33 +11,29 @@ const validate = async (event) => {
   }
 }
 
-//check for valid input then
+//calculate average if input is valid
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
   if (document.querySelector('#grade1').checkValidity() && document.querySelector('#grade2').checkValidity() && document.querySelector('#grade3').checkValidity()) {
-    const b = parseInt(document.querySelector('#grade1').value)
-    const b = parseInt(document.querySelector('#grade2').value)
-    const c = parseInt(document.querySelector('#grade3').value)
-    const ans = `Your average grade is is ${average(a, b, c)}.`
+    const a = Number(document.querySelector('#grade1').value)
+    const b = Number(document.querySelector('#grade2').value)
+    const c = Number(document.querySelector('#grade3').value)
+    const ans = `Your average grade is ${average(a, b, c).toFixed(2)}.`
     document.querySelector('#result').innerHTML = ans
   }
 }
-
 
 // delegate to dynamic elements (e.g. when testing)
 // focusout is like blur, but it bubbles up
 
 document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
+  if ((event.target && event.target.id === 'grade1') ||
+      (event.target && event.target.id === 'grade2') ||
+      (event.target && event.target.id === 'grade3')) {
     validate(event)
   }
 })
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
-})
-
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
+  if (event.target && event.target.id === 'calculate') { updateWithAdd(event) }
 })
